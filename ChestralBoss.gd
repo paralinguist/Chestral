418b5ba024bd2b59c6b697955c96b4125af1da48
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2021-11-23 21:47:49 +0800

    Spaces
    Converted Tabs to Spaces >:(

--- ChestralBoss.gd
+++ ChestralBoss.gd
@@ -8,26 +8,26 @@ export (int) var base_interference = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ProgressBar.max_value = misalignment
-	$ProgressBar.value = misalignment
-	
+    $ProgressBar.max_value = misalignment
+    $ProgressBar.value = misalignment
+    
 func realign(align_value):
-	interference = interference - align_value
-	var damage_done = 0
-	if interference < 0:
-		misalignment = misalignment + interference
-		damage_done = interference
-		interference = 0
-	#Should probably play some flappy animation?
-	return damage_done
+    interference = interference - align_value
+    var damage_done = 0
+    if interference < 0:
+        misalignment = misalignment + interference
+        damage_done = interference
+        interference = 0
+    #Should probably play some flappy animation?
+    return damage_done
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$ProgressBar.value = misalignment
+    $ProgressBar.value = misalignment
 
 
 func _on_Timer_timeout():
-	if $Sprite.animation == "Idle":
-		$Sprite.animation = "Flap"
-	else:
-		$Sprite.animation = "Idle"
+    if $Sprite.animation == "Idle":
+        $Sprite.animation = "Flap"
+    else:
+        $Sprite.animation = "Idle"
