0e6049af2ffc734179184acc9be26697e34f13e8
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2021-11-23 21:40:31 +0800

    Animated Birb
    Cycles between flapping and non flapping every 3 seconds

--- ChestralServer.gd
+++ ChestralServer.gd
@@ -17,170 +17,170 @@ var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 #Some weird magic numbers in here - need to lock down the polygon geometry
 func arrange_musicians():
-    var radius = 350
-    var sides = len(clients)
-    if sides > 0:
-        var angle = -1 * 360 / sides * 2.5
-        var x = radius * cos(angle) + radius * 2.2
-        var y = radius * sin(angle) + radius * 0.9
-        #print(str(x) +','+ str(y))
-        var line = 1
-        for client_id in clients:
-            var client = clients[client_id]
-            client.reposition(x,y)
-            x = radius * cos(angle + line * 2 * PI / sides) + radius * 2.2
-            y = radius * sin(angle + line * 2 * PI / sides) + radius * 0.9
-            line = line + 1
-            #print(str(x) +','+ str(y))
+	var radius = 350
+	var sides = len(clients)
+	if sides > 0:
+		var angle = -1 * 360 / sides * 2.5
+		var x = radius * cos(angle) + radius * 2.2
+		var y = radius * sin(angle) + radius * 0.9
+		#print(str(x) +','+ str(y))
+		var line = 1
+		for client_id in clients:
+			var client = clients[client_id]
+			client.reposition(x,y)
+			x = radius * cos(angle + line * 2 * PI / sides) + radius * 2.2
+			y = radius * sin(angle + line * 2 * PI / sides) + radius * 0.9
+			line = line + 1
+			#print(str(x) +','+ str(y))
 
 func create_musician(name, instrument, id):
-    var musician = Musician.instance()
-    musician.train(name, instrument, id)
-    add_child(musician)
-    return musician
+	var musician = Musician.instance()
+	musician.train(name, instrument, id)
+	add_child(musician)
+	return musician
 
 func _ready():
-    # Connect base signals to get notified of new client connections,
-    # disconnections, and disconnect requests.
-    _server.connect("client_connected", self, "_connected")
-    _server.connect("client_disconnected", self, "_disconnected")
-    _server.connect("client_close_request", self, "_close_request")
-    # This signal is emitted when not using the Multiplayer API every time a
-    # full packet is received.
-    # Alternatively, you could check get_peer(PEER_ID).get_available_packets()
-    # in a loop for each connected peer.
-    _server.connect("data_received", self, "_on_data")
-    # Start listening on the given port.
-    var err = _server.listen(PORT)
-    if err != OK:
-        print("Unable to start server")
-        set_process(false)
-    $Panel/LabelIPs.text = 'Server IP/s: '
-    var ip
-    for address in IP.get_local_addresses():
-        if (address.split('.').size() == 4) and not (address.begins_with('169') or address.begins_with('127')):
-            $Panel/LabelIPs.text += ' ( ' + address + ' ) '
-    
-    rng.randomize()
-    
+	# Connect base signals to get notified of new client connections,
+	# disconnections, and disconnect requests.
+	_server.connect("client_connected", self, "_connected")
+	_server.connect("client_disconnected", self, "_disconnected")
+	_server.connect("client_close_request", self, "_close_request")
+	# This signal is emitted when not using the Multiplayer API every time a
+	# full packet is received.
+	# Alternatively, you could check get_peer(PEER_ID).get_available_packets()
+	# in a loop for each connected peer.
+	_server.connect("data_received", self, "_on_data")
+	# Start listening on the given port.
+	var err = _server.listen(PORT)
+	if err != OK:
+		print("Unable to start server")
+		set_process(false)
+	$Panel/LabelIPs.text = 'Server IP/s: '
+	var ip
+	for address in IP.get_local_addresses():
+		if (address.split('.').size() == 4) and not (address.begins_with('169') or address.begins_with('127')):
+			$Panel/LabelIPs.text += ' ( ' + address + ' ) '
+	
+	rng.randomize()
+	
 func _connected(id, proto):
-    # This is called when a new peer connects, "id" will be the assigned peer id,
-    # "proto" will be the selected WebSocket sub-protocol (which is optional)
-    var cnx = "Client %d connected with protocol: %s" % [id, proto]
-    $Panel/LabelConnections.text = cnx
+	# This is called when a new peer connects, "id" will be the assigned peer id,
+	# "proto" will be the selected WebSocket sub-protocol (which is optional)
+	var cnx = "Client %d connected with protocol: %s" % [id, proto]
+	$Panel/LabelConnections.text = cnx
 
 func _close_request(id, code, reason):
-    # This is called when a client notifies that it wishes to close the connection,
-    # providing a reason string and close code.
-    var cnx = "Client %d disconnecting with code: %d, reason: %s" % [id, code, reason]
-    $Panel/LabelConnections.text = cnx
+	# This is called when a client notifies that it wishes to close the connection,
+	# providing a reason string and close code.
+	var cnx = "Client %d disconnecting with code: %d, reason: %s" % [id, code, reason]
+	$Panel/LabelConnections.text = cnx
 
 func _disconnected(id, was_clean = false):
-    # This is called when a client disconnects, "id" will be the one of the
-    # disconnecting client, "was_clean" will tell you if the disconnection
-    # was correctly notified by the remote peer before closing the socket.
-    var cnx = "Client %d disconnected, clean: %s" % [id, str(was_clean)]
-    $Panel/LabelConnections.text = cnx
-    if id in clients:
-        $Panel/MessageLog.text += clients[id].playername + ' has left the server.\n'
-        remove_child(clients[id])
-        clients.erase(id)
-        players.remove(players.find(id))
-        arrange_musicians()
-    $Panel/MessageLog.scroll_vertical=INF
+	# This is called when a client disconnects, "id" will be the one of the
+	# disconnecting client, "was_clean" will tell you if the disconnection
+	# was correctly notified by the remote peer before closing the socket.
+	var cnx = "Client %d disconnected, clean: %s" % [id, str(was_clean)]
+	$Panel/LabelConnections.text = cnx
+	if id in clients:
+		$Panel/MessageLog.text += clients[id].playername + ' has left the server.\n'
+		remove_child(clients[id])
+		clients.erase(id)
+		players.remove(players.find(id))
+		arrange_musicians()
+	$Panel/MessageLog.scroll_vertical=INF
 
 #Returns the id of the next muso in the circle. If last, returns first.
 func get_adjacent(id):
-    var adjacent = players.find(id) + 1
-    if adjacent >= len(players):
-        adjacent = 0
-    return players[adjacent]
+	var adjacent = players.find(id) + 1
+	if adjacent >= len(players):
+		adjacent = 0
+	return players[adjacent]
 
 func send_all_client_data(id):
-    var client_states = {}
-    for client in clients:
-        client_states[client] = clients[client].get_state()
-    var client_states_string = 'allcli|' + JSON.print(client_states)
-    _server.get_peer(id).put_packet(client_states_string.to_utf8())
-        
+	var client_states = {}
+	for client in clients:
+		client_states[client] = clients[client].get_state()
+	var client_states_string = 'allcli|' + JSON.print(client_states)
+	_server.get_peer(id).put_packet(client_states_string.to_utf8())
+		
 
 func _on_data(id):
-    # Print the received packet, you MUST always use get_peer(id).get_packet to receive data,
-    # and not get_packet directly when not using the MultiplayerAPI.
-    var pkt = _server.get_peer(id).get_packet()
-    var incoming = pkt.get_string_from_utf8()
-    var message = 'kay'
-    $Panel/LabelStatus.text = incoming
-    print("Got data from client %d: %s ... echoing" % [id, pkt.get_string_from_utf8()])
-    #_server.get_peer(id).put_packet(pkt)
-    if incoming.begins_with('||:'):
-        clients[id] = create_musician(incoming.right(3), 'Violin', id)
-        players.append(id)
-        arrange_musicians()
-        $Panel/MessageLog.text += incoming.right(3) + ' has entered the server.\n'
-        $Panel/MessageLog.scroll_vertical=INF
-        message = '||:' + str(id)
-    else:
-        if id in clients:
-            $Panel/MessageLog.text += clients[id].playername + ': ' + incoming + "\n"
-            $Panel/MessageLog.scroll_vertical=INF
-        else:
-            incoming = ''
-    if incoming:
-        _server.get_peer(id).put_packet(message.to_utf8())
-        if incoming.begins_with('align|'):
-            var align_value = int(incoming.right(6))
-            #Should pass id so boss can calculate resonance/harmonics
-            var align_done = $ChestralBoss.realign(align_value)
-            $Panel/MessageLog.text += clients[id].playername + ' aligned: ' + str(align_done) + '\n'
-        elif incoming.begins_with('aligm|'):
-            var align_value = incoming.right(6)
-            align_value = align_value.split('x')
-            var align_mag = align_value[0]
-            var times = align_value[1]
-            var align_done = $ChestralBoss.realign(align_mag, times)
-        elif incoming.begins_with('inter|'):
-            var inter_value = int(incoming.right(6))
-            clients[id].apply_int(inter_value)
-        elif incoming.begins_with('adjin|'):
-            var inter_value = int(incoming.right(6))
-            clients[get_adjacent(id)].apply_int(inter_value)
-        elif incoming.begins_with('sooth|'):
-            var soothe_value = int(incoming.right(6))
-            clients[get_adjacent(id)].apply_soothe(soothe_value, false)
-        elif incoming.begins_with('reson|'):
-            var reson_value = int(incoming.right(6))
-            clients[get_adjacent(id)].apply_resonance(reson_value)
-        elif incoming.begins_with('harmo|'):
-            var harmo_value = int(incoming.right(6))
-            clients[get_adjacent(id)].apply_harmonics(harmo_value)
-        elif incoming.begins_with('sooae|'):
-            var soothe_value = int(incoming.right(6))
-            for client in clients:
-                clients[client].apply_soothe(soothe_value, true)
-        elif incoming.begins_with('avata|'):
-            clients[id].set_avatar(incoming.right(6))
-        elif incoming.begins_with('maest|'):
-            send_all_client_data(id)
+	# Print the received packet, you MUST always use get_peer(id).get_packet to receive data,
+	# and not get_packet directly when not using the MultiplayerAPI.
+	var pkt = _server.get_peer(id).get_packet()
+	var incoming = pkt.get_string_from_utf8()
+	var message = 'kay'
+	$Panel/LabelStatus.text = incoming
+	print("Got data from client %d: %s ... echoing" % [id, pkt.get_string_from_utf8()])
+	#_server.get_peer(id).put_packet(pkt)
+	if incoming.begins_with('||:'):
+		clients[id] = create_musician(incoming.right(3), 'Violin', id)
+		players.append(id)
+		arrange_musicians()
+		$Panel/MessageLog.text += incoming.right(3) + ' has entered the server.\n'
+		$Panel/MessageLog.scroll_vertical=INF
+		message = '||:' + str(id)
+	else:
+		if id in clients:
+			$Panel/MessageLog.text += clients[id].playername + ': ' + incoming + "\n"
+			$Panel/MessageLog.scroll_vertical=INF
+		else:
+			incoming = ''
+	if incoming:
+		_server.get_peer(id).put_packet(message.to_utf8())
+		if incoming.begins_with('align|'):
+			var align_value = int(incoming.right(6))
+			#Should pass id so boss can calculate resonance/harmonics
+			var align_done = $ChestralBoss.realign(align_value)
+			$Panel/MessageLog.text += clients[id].playername + ' aligned: ' + str(align_done) + '\n'
+		elif incoming.begins_with('aligm|'):
+			var align_value = incoming.right(6)
+			align_value = align_value.split('x')
+			var align_mag = align_value[0]
+			var times = align_value[1]
+			var align_done = $ChestralBoss.realign(align_mag, times)
+		elif incoming.begins_with('inter|'):
+			var inter_value = int(incoming.right(6))
+			clients[id].apply_int(inter_value)
+		elif incoming.begins_with('adjin|'):
+			var inter_value = int(incoming.right(6))
+			clients[get_adjacent(id)].apply_int(inter_value)
+		elif incoming.begins_with('sooth|'):
+			var soothe_value = int(incoming.right(6))
+			clients[get_adjacent(id)].apply_soothe(soothe_value, false)
+		elif incoming.begins_with('reson|'):
+			var reson_value = int(incoming.right(6))
+			clients[get_adjacent(id)].apply_resonance(reson_value)
+		elif incoming.begins_with('harmo|'):
+			var harmo_value = int(incoming.right(6))
+			clients[get_adjacent(id)].apply_harmonics(harmo_value)
+		elif incoming.begins_with('sooae|'):
+			var soothe_value = int(incoming.right(6))
+			for client in clients:
+				clients[client].apply_soothe(soothe_value, true)
+		elif incoming.begins_with('avata|'):
+			clients[id].set_avatar(incoming.right(6))
+		elif incoming.begins_with('maest|'):
+			send_all_client_data(id)
 
-        $Panel/MessageLog.scroll_vertical=INF
+		$Panel/MessageLog.scroll_vertical=INF
 
 func _process(delta):
-    # Call this in _process or _physics_process.
-    # Data transfer, and signals emission will only happen when calling this function.
-    _server.poll()
+	# Call this in _process or _physics_process.
+	# Data transfer, and signals emission will only happen when calling this function.
+	_server.poll()
 
 
 func _on_ServerDataPulse_timeout():
-    for client in clients:
-        var client_state = 'state|' + JSON.print(clients[client].get_state())
-        _server.get_peer(client).put_packet(client_state.to_utf8())
+	for client in clients:
+		var client_state = 'state|' + JSON.print(clients[client].get_state())
+		_server.get_peer(client).put_packet(client_state.to_utf8())
 
 #Debugging tools
 func _on_Button_pressed():
-    var victim = clients[players[randi() % players.size()]]
-    var dissonance = rng.randi_range(10,50)
-    print('Squarked at ' + victim.playername + ' for: ' + str(victim.irritate(dissonance)))
+	var victim = clients[players[randi() % players.size()]]
+	var dissonance = rng.randi_range(10,50)
+	print('Squarked at ' + victim.playername + ' for: ' + str(victim.irritate(dissonance)))
 
 func _on_Unbalance_pressed():
-    $ChestralBoss.misalignment += 500
+	$ChestralBoss.misalignment += 500
--- Musician.gd
+++ Musician.gd
@@ -16,169 +16,169 @@ var image
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $Sprite.scale = Vector2(0.5,0.5)
-    $Sprite.position.y = $Sprite.position.y + 200
+	$Sprite.scale = Vector2(0.5,0.5)
+	$Sprite.position.y = $Sprite.position.y + 200
 
 func reposition(new_x,new_y):
-    self.position.x = new_x
-    self.position.y = new_y
+	self.position.x = new_x
+	self.position.y = new_y
 
 #directly changes interference + label bar
 func set_interference(inter_value, additive):
-    if additive:
-        interference = interference + inter_value
-        if interference <= 0:
-            interference = 0
-        else:
-            $Interference/InterferenceLabel.visible = true
-            $Interference.visible = true
-    else:
-        interference = inter_value
-    $Interference/InterferenceLabel.text = str(interference)
+	if additive:
+		interference = interference + inter_value
+		if interference <= 0:
+			interference = 0
+		else:
+			$Interference/InterferenceLabel.visible = true
+			$Interference.visible = true
+	else:
+		interference = inter_value
+	$Interference/InterferenceLabel.text = str(interference)
 
 #directly changes harmonics + label bar
 func set_harmonics(harmonics_value, additive):
-    if additive:
-        harmonics = harmonics + harmonics_value
-        if harmonics <= 0:
-            harmonics = 0
-        else:
-            $Harmonics/HarmonicsLabel.visible = true
-            $Harmonics.visible = true
-    else:
-        harmonics = harmonics_value
-    $Harmonics/HarmonicsLabel.text = str(harmonics)
+	if additive:
+		harmonics = harmonics + harmonics_value
+		if harmonics <= 0:
+			harmonics = 0
+		else:
+			$Harmonics/HarmonicsLabel.visible = true
+			$Harmonics.visible = true
+	else:
+		harmonics = harmonics_value
+	$Harmonics/HarmonicsLabel.text = str(harmonics)
 
 #directly changes resonance + label bar
 func set_resonance(resonance_value, additive):
-    if additive:
-        resonance = resonance + resonance_value
-        if resonance <= 0:
-            resonance = 0
-        else:
-            $Resonance/ResonanceLabel.visible = true
-            $Resonance.visible = true
-    else:
-        resonance = resonance_value
-    $Resonance/ResonanceLabel.text = str(resonance)
+	if additive:
+		resonance = resonance + resonance_value
+		if resonance <= 0:
+			resonance = 0
+		else:
+			$Resonance/ResonanceLabel.visible = true
+			$Resonance.visible = true
+	else:
+		resonance = resonance_value
+	$Resonance/ResonanceLabel.text = str(resonance)
 
 #Applies interference with a timer
 func apply_int(inter_value):
-    set_interference(inter_value, true)
-    $IntTimer.start()
+	set_interference(inter_value, true)
+	$IntTimer.start()
 
 #Applies harmonics with a timer
 func apply_harmonics(harmonics_value):
-    set_harmonics(harmonics_value, true)
-    $HarmonicsTimer.start()
-    
+	set_harmonics(harmonics_value, true)
+	$HarmonicsTimer.start()
+	
 #Applies resonance with a timer
 func apply_resonance(resonance_value):
-    set_resonance(resonance_value, true)
-    $ResonanceTimer.start()
+	set_resonance(resonance_value, true)
+	$ResonanceTimer.start()
 
 #directly changes soothe + label bar
 func set_soothe(soothe_value, additive):
-    if additive:
-        soothe = soothe + soothe_value
-        if soothe <= 0:
-            soothe = 0
-        else:
-            $Soothe/SootheLabel.visible = true
-            $Soothe.visible = true
-    else:
-        soothe = soothe_value
-    $Soothe/SootheLabel.text = str(soothe)
+	if additive:
+		soothe = soothe + soothe_value
+		if soothe <= 0:
+			soothe = 0
+		else:
+			$Soothe/SootheLabel.visible = true
+			$Soothe.visible = true
+	else:
+		soothe = soothe_value
+	$Soothe/SootheLabel.text = str(soothe)
 
 #Apply an int of soothe to muso - instant is a bool, is this instant or applied on timer?
 func apply_soothe(soothe_value, instant):
-    if instant:
-        current_irritation = current_irritation - soothe_value
-        if current_irritation < 0:
-            current_irritation = 0
-    else:
-        set_soothe(soothe_value, true)
-        $SootheTimer.start()
-    
+	if instant:
+		current_irritation = current_irritation - soothe_value
+		if current_irritation < 0:
+			current_irritation = 0
+	else:
+		set_soothe(soothe_value, true)
+		$SootheTimer.start()
+	
 func train(name, instrument, id):
-    playername = name
-    $Label.text = name
-    connection_id = id
-    image = load("res://Sprites/Instruments/sax.png")
-    if instrument == 'Violin':
-        max_irritation = 200
-        base_align = 10
-        image = load("res://Sprites/Instruments/violin.png")  
-    $Sprite.texture = image
+	playername = name
+	$Label.text = name
+	connection_id = id
+	image = load("res://Sprites/Instruments/sax.png")
+	if instrument == 'Violin':
+		max_irritation = 200
+		base_align = 10
+		image = load("res://Sprites/Instruments/violin.png")  
+	$Sprite.texture = image
 
 func set_avatar(filename):
-    var avatar_file = 'res://Sprites/Orcs/' + filename
-    if ResourceLoader.exists(avatar_file):
-        image = load(avatar_file)
-        $Sprite.texture = image
+	var avatar_file = 'res://Sprites/Orcs/' + filename
+	if ResourceLoader.exists(avatar_file):
+		image = load(avatar_file)
+		$Sprite.texture = image
 
 #TODO: check for death
 func _process(delta):
-    $Irritation.value = current_irritation
-    $Soothe.value = $SootheTimer.time_left
-    $Interference.value = $IntTimer.time_left
-    $Harmonics.value = $HarmonicsTimer.time_left
-    $Resonance.value = $ResonanceTimer.time_left
-    if soothe <= 0:
-        $Soothe/SootheLabel.visible = false
-        $Soothe.visible = false
-    if interference <= 0:
-        $Interference/InterferenceLabel.visible = false
-        $Interference.visible = false
-    if harmonics <= 0:
-        $Harmonics/HarmonicsLabel.visible = false
-        $Harmonics.visible = false
-    if resonance <= 0:
-        $Resonance/ResonanceLabel.visible = false
-        $Resonance.visible = false
-        
-    $Irritation.value = current_irritation
-    #Need to check for "dead" player here and handle
+	$Irritation.value = current_irritation
+	$Soothe.value = $SootheTimer.time_left
+	$Interference.value = $IntTimer.time_left
+	$Harmonics.value = $HarmonicsTimer.time_left
+	$Resonance.value = $ResonanceTimer.time_left
+	if soothe <= 0:
+		$Soothe/SootheLabel.visible = false
+		$Soothe.visible = false
+	if interference <= 0:
+		$Interference/InterferenceLabel.visible = false
+		$Interference.visible = false
+	if harmonics <= 0:
+		$Harmonics/HarmonicsLabel.visible = false
+		$Harmonics.visible = false
+	if resonance <= 0:
+		$Resonance/ResonanceLabel.visible = false
+		$Resonance.visible = false
+		
+	$Irritation.value = current_irritation
+	#Need to check for "dead" player here and handle
 
 func _on_IntTimer_timeout():
-    set_interference(0, false)
+	set_interference(0, false)
 
 func _on_SootheTimer_timeout():
-    current_irritation = current_irritation - soothe
-    if current_irritation < 0:
-        current_irritation = 0
-    set_soothe(0, false)
+	current_irritation = current_irritation - soothe
+	if current_irritation < 0:
+		current_irritation = 0
+	set_soothe(0, false)
 
 func _on_HarmonicsTimer_timeout():
-    set_harmonics(0, false)
+	set_harmonics(0, false)
 
 func _on_ResonanceTimer_timeout():
-    set_resonance(0, false)
+	set_resonance(0, false)
 
 #Send image filename too?
 func get_state():
-    var state = {}
-    state['id'] = connection_id
-    state['name'] = playername
-    state['instrument'] = instrument
-    state['max_irritation'] = max_irritation
-    state['irritation'] = current_irritation
-    state['interference'] = interference
-    state['soothe'] = soothe
-    state['resonance'] = resonance
-    state['harmonics'] = harmonics
-    state['interference_time'] = $Interference.value
-    state['soothe_time'] = $Soothe.value
-    state['resonance_time'] = $Resonance.value
-    state['harmonics_time'] = $Harmonics.value
-    return state
+	var state = {}
+	state['id'] = connection_id
+	state['name'] = playername
+	state['instrument'] = instrument
+	state['max_irritation'] = max_irritation
+	state['irritation'] = current_irritation
+	state['interference'] = interference
+	state['soothe'] = soothe
+	state['resonance'] = resonance
+	state['harmonics'] = harmonics
+	state['interference_time'] = $Interference.value
+	state['soothe_time'] = $Soothe.value
+	state['resonance_time'] = $Resonance.value
+	state['harmonics_time'] = $Harmonics.value
+	return state
 
 func irritate(dissonance):
-    interference = interference - dissonance
-    var damage_done = 0
-    if interference < 0:
-        current_irritation = current_irritation - interference
-        damage_done = interference
-        interference = 0
-    #Should animate over this muso to show they got annoyed
-    return damage_done
+	interference = interference - dissonance
+	var damage_done = 0
+	if interference < 0:
+		current_irritation = current_irritation - interference
+		damage_done = interference
+		interference = 0
+	#Should animate over this muso to show they got annoyed
+	return damage_done
